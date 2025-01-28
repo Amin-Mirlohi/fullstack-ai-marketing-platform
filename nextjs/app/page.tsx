987358aa18b8222e3936cd2b56842cda7f7874cc
Amin <sammirlohi@gmail.com>
2025-01-28 14:16:20 -0500

    Add Public directory

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <div className="bg-red-500 text-white">Hello project</div>;
       <Button variant={"secondary"}>Click me</Button>
+      <div className="bg-slate-900">Buy!!</div>
     </div>
   );
 }
