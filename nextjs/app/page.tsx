9c1509de64263fb9f3c7d51793bd1f5f5db3bd30
Amin <amin@yappybuy.com>
2025-01-28 12:36:47 -0500

    first commit

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">Hello project</div>;
+      <Button variant={"secondary"}>Click me</Button>
+    </div>
+  );
+}
